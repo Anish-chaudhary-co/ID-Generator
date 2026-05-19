ae99a46aeb545a3e84bc05677b7f9000903e4dd1
Anish-chaudhary <anishuna777@gmail.com>
2026-05-19 23:01:12 +0545

    Add ID card photo

--- src/Pages/Templates.jsx
+++ src/Pages/Templates.jsx
@@ -12,11 +12,11 @@ const Templates = () => {
   return (
  <>
  <div className='grid grid-cols-1 md:grid-cols-3 md:m-9'>
- <div className='w-93 md:w-85 py-55 bg-gray-400 m-2 border border-gray-50 shadow-2xl rounded-lg'><img className='h-full w-full object-cover' src={Templates1} alt="Template" /></div>
- <div className='w-93 md:w-97 py-55 bg-gray-400 m-2 border border-gray-50 shadow-2xl rounded-lg'><img className='h-full w-full ' src={Templates2} alt="Templates" /></div>
- <div className='w-93 md:w-85 py-55 bg-gray-400 m-2 border border-gray-50 shadow-2xl rounded-lg'><img className='h-full w-full object-cover' src={Templates3} alt="Templates" /></div>
- <div className='w-93 md:w-85 py-55 bg-gray-400 m-2 border border-gray-50 shadow-2xl rounded-lg'><img className='h-full w-full object-cover' src={Templates4} alt="Templates" /></div>
- <div className='w-93 md:w-85 py-55 bg-gray-400 m-2 border border-gray-50 shadow-2xl rounded-lg'><img className='h-full w-full object-cover' src={Templates5} alt="Templates" /></div>
+ <div className=' bg-gray-400 m-2 border border-gray-50 shadow-2xl rounded-lg'><img className='h-full w-full object-cover' src={Templates1} alt="Template" /></div>
+ <div className=' bg-gray-400 m-2 border border-gray-50 shadow-2xl rounded-lg'><img className='h-full w-full ' src={Templates4} alt="Templates" /></div>
+ <div className=' bg-gray-400 m-2 border border-gray-50 shadow-2xl rounded-lg'><img className='h-full w-full object-cover' src={Templates3} alt="Templates" /></div>
+ <div className=' bg-gray-400 m-2 border border-gray-50 shadow-2xl rounded-lg'><img className='h-full w-full object-cover' src={Templates2} alt="Templates" /></div>
+ <div className=' bg-gray-400 m-2 border border-gray-50 shadow-2xl rounded-lg'><img className='h-full w-full object-cover' src={Templates5} alt="Templates" /></div>
  </div>
  </>
   )
