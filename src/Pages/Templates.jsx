23bbabf586516965e03103fd2c542bf1bbf311cd
Anish-chaudhary <anishuna777@gmail.com>
2026-05-19 18:44:39 +0545

    add ID card

--- src/Pages/Templates.jsx
+++ src/Pages/Templates.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import Templates1 from '../assets/templates1.png';
+import Templates2 from '../assets/templates2.png';
+import Templates3 from '../assets/templates3.png';
+import Templates4 from '../assets/templates4.png';
+import Templates5 from '../assets/templates5.png';
+
+
 const Templates = () => {
   return (
  <>
- <div className='grid md:grid-col-4'>
- <div className='w-90 md:width[100px] py-60 m-4 border border-gray-50 shadow-2xl'>this is the template</div>
- <div className='w-90 md:width[100px] py-60 m-4 border border-gray-50 shadow-2xl'>this is the template</div>
- <div className='w-90 md:width[100px] py-60 m-4 border border-gray-50 shadow-2xl'>this is the template</div>
- <div className='w-90 md:width[100px] py-60 m-4 border border-gray-50 shadow-2xl'>this is the template</div>
- <div className='w-90 md:width[100px] py-60 m-4 border border-gray-50 shadow-2xl'>this is the template</div>
- <div className='w-90 md:width[100px] py-60 m-4 border border-gray-50 shadow-2xl'>this is the template</div>
+ <div className='grid grid-cols-1 md:grid-cols-3 md:m-9'>
+ <div className='w-93 md:w-85 py-55 bg-gray-400 m-2 border border-gray-50 shadow-2xl rounded-lg'><img className='h-full w-full object-cover' src={Templates1} alt="Template" /></div>
+ <div className='w-93 md:w-97 py-55 bg-gray-400 m-2 border border-gray-50 shadow-2xl rounded-lg'><img className='h-full w-full ' src={Templates2} alt="Templates" /></div>
+ <div className='w-93 md:w-85 py-55 bg-gray-400 m-2 border border-gray-50 shadow-2xl rounded-lg'><img className='h-full w-full object-cover' src={Templates3} alt="Templates" /></div>
+ <div className='w-93 md:w-85 py-55 bg-gray-400 m-2 border border-gray-50 shadow-2xl rounded-lg'><img className='h-full w-full object-cover' src={Templates4} alt="Templates" /></div>
+ <div className='w-93 md:w-85 py-55 bg-gray-400 m-2 border border-gray-50 shadow-2xl rounded-lg'><img className='h-full w-full object-cover' src={Templates5} alt="Templates" /></div>
  </div>
  </>
   )
