655b745ab8d2a4b32baa431b03c8e9cc83a31688
Anish-chaudhary <anishuna777@gmail.com>
2026-05-20 21:47:13 +0545

    make Templates conponent responsive

--- src/Pages/Templates.jsx
+++ src/Pages/Templates.jsx
@@ -1,25 +1,53 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
-import Templates1 from '../assets/templates1.png';
-import Templates2 from '../assets/templates2.png';
-import Templates3 from '../assets/templates3.png';
-import Templates4 from '../assets/templates4.png';
-import Templates5 from '../assets/templates5.png';
-
+import Templates1 from "../assets/templates1.png";
+import Templates2 from "../assets/templates2.png";
+import Templates3 from "../assets/templates3.png";
+import Templates4 from "../assets/templates4.png";
+import Templates5 from "../assets/templates5.png";
 
 const Templates = () => {
   return (
- <>
- <div className='grid grid-cols-1 md:grid-cols-3 md:m-9'>
- <div className=' bg-gray-400 m-2 border border-gray-50 shadow-2xl rounded-lg'><img className='h-full w-full object-cover' src={Templates1} alt="Template" /></div>
- <div className=' bg-gray-400 m-2 border border-gray-50 shadow-2xl rounded-lg'><img className='h-full w-full ' src={Templates4} alt="Templates" /></div>
- <div className=' bg-gray-400 m-2 border border-gray-50 shadow-2xl rounded-lg'><img className='h-full w-full object-cover' src={Templates3} alt="Templates" /></div>
- <div className=' bg-gray-400 m-2 border border-gray-50 shadow-2xl rounded-lg'><img className='h-full w-full object-cover' src={Templates2} alt="Templates" /></div>
- <div className=' bg-gray-400 m-2 border border-gray-50 shadow-2xl rounded-lg'><img className='h-full w-full object-cover' src={Templates5} alt="Templates" /></div>
- </div>
- </>
-  )
-}
+    <>
+    <div className="m-4">
+      <h1 className="text-4xl font-bold">Templates</h1>
+    </div>
+      <div className="grid grid-cols-1 md:grid-cols-3 md:m-9">
+        <div className="bg-gray-400 m-2 border border-gray-50 shadow-2xl rounded-lg overflow-hidden md:overflow-hidden h-[35rem] md:h-[35rem] md:w-90">
+          <img
+            className="h-full w-full object-cover"
+            src={Templates1}
+            alt="Template"
+          />
+        </div>
+        <div className="bg-gray-400 m-2 border border-gray-50 shadow-2xl rounded-lg overflow-hidden md:overflow-hidden h-[20rem] md:h-[25rem]">
+          <img className="h-full w-full " src={Templates4} alt="Templates" />
+        </div>
+        <div className="bg-gray-400 m-2 border border-gray-50 shadow-2xl rounded-lg overflow-hidden md:overflow-hidden md:h-[40rem] md:w-90">
+          <img
+            className="h-full w-full object-cover"
+            src={Templates3}
+            alt="Templates"
+          />
+        </div>
+        <div className="bg-gray-400 m-2 border border-gray-50 shadow-2xl rounded-lg overflow-hidden md:overflow-hidden h-[15rem] md:h-[20rem]">
+          <img
+            className="h-full w-full object-cover"
+            src={Templates2}
+            alt="Templates"
+          />
+        </div>
+        <div className="bg-gray-400 m-2 border border-gray-50 shadow-2xl rounded-lg overflow-hidden md:overflow-hidden h-[15rem] md:h-[20rem]">
+          <img
+            className="h-full w-full object-cover"
+            src={Templates5}
+            alt="Templates"
+          />
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Templates;
