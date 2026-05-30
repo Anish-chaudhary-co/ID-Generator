ce7e1f0c09cb0a5086777bafddaf14e9936314d4
Anish-chaudhary <anishuna777@gmail.com>
2026-05-30 12:09:58 +0545

    make template preview and use that template

--- src/Pages/Templates.jsx
+++ src/Pages/Templates.jsx
@@ -10,39 +10,54 @@ import selectedTemplate from "../Templates/Templates";
 // { handleTemplateSelect }
 
 const Templates = ({ handleTemplateClick }) => {
-  // const handleTemplateClick = (template) => {
-  //   handleTemplateSelect(template);
-  // };
+const [templatePreview, setTemplatePreview] = useState(null);
 
   const handleClick = (item) => {
     handleTemplateClick(item);
   };
 
-  // const [takeDetail,setTakeDetail] = useState(null);
-  // const handleTemplateClick =(template)=>{
-  //     setTakeDetail(template);
-  // }
-
   return (
     <>
       <div className="grid grid-cols-1 md:grid-cols-3 mt-10 md:ml-14">
         {selectedTemplate.map((template, index) => (
-          <div key={index} className="gap-4 border m-4 p-2 w-90 md:w-97 h-99 md:h-90 rounded-lg shadow-lg relative">
-            <h1 className="text-2xl font-bold">{template.templateName}</h1>
-            <button className="absolute bottom-3 left-6 bg-green-500 hover:bg-green-600 text-white p-2 rounded-sm">Preview</button>
-            <Link key={index} to="/personalDetailed">
-              <button
-                onClick={() => {
-                  handleClick(template);
-                }}
-                className="absolute bottom-3 right-6 bg-green-500 hover:bg-green-600 text-white p-2 rounded-sm"
-              >
-                Create Id
-              </button>
-            </Link>
+          <div key={index} className="relative gap-4 border m-4 p-2 py-60 rounded-lg shadow-lg"
+          onClick={() => setTemplatePreview(template)}
+          >
+            <h1 className="absolute top-2 text-2xl font-bold">{template.templateName}</h1>
+            
           </div>
         ))}
       </div>
+      {
+        templatePreview && (
+          <div 
+          onClick={()=>setTemplatePreview(null)}
+          className="fixed inset-0 bg-black/50 bg-opacity-50 flex justify-center items-center">
+            <div 
+            onClick={(e)=>e.stopPropagation()}
+            className="relative bg-white p-4 rounded-lg shadow-lg py-15 ">
+              <div className="flex justify-between items-center mb-4">
+              <h2 className="font-bold text-2xl">{templatePreview.templateName}</h2>
+              <button
+              className="bg-red-500 hover:bg-red-600 text-white text-xl font-bold px-4 py-2 rounded-sm"
+              onClick={()=>setTemplatePreview(null)}>X</button>
+              </div>
+              <div className="border m-2 p-2 w-70 md:w-97 h-99 md:h-90 rounded-lg shadow-lg">
+                here we will show the preview of template
+              </div>
+
+              <div>
+                <Link to="/personalDetailed">
+                <button className="absolute bottom-2 right-4 bg-blue-500 hover:bg-blue-600 text-white text-xl font-bold px-4 py-2 rounded-sm">
+                  Use Template
+                </button>
+                </Link>
+              </div>
+
+            </div>
+          </div>
+        )
+      }
     </>
   );
 };
